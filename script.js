b9f34fb8292fee23fce1be2d62ae232c44afd625
DimarcoKevin <56969581+DimarcoKevin@users.noreply.github.com>
2021-05-25 19:35:04 -0400

    dynamically creating question board with jquery

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+
+    var questions = 8
+    var answers = 3
     
     // main function to use jquery to append questions
     $(".page").append(
@@ -6,6 +9,18 @@ $(document).ready(function() {
         <label for="name">Name:</label>
         <input type="text" id="name" name="name"><br><br>
         </div>`
-
     )
+
+    for (var i = 1; i <= questions; i++) {   
+        $(".page").append(
+            `<div class="Question" id="Q${i}">
+            <h3>Question ${i}: </h3>`
+        )
+        for (var j = 1; j <= answers; j++) {
+            $(".page").append(
+            `<label for="Q${i}A${j}">Answer ${j}:</label>
+            <input type="text" id="Q${i}A${j}" name="Q${i}A${j}"><br><br>`
+            )
+        }
+    }
 });
