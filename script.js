f0ce8d85ac97bbd429280fcd86cdb80d8d833661
DimarcoKevin <56969581+DimarcoKevin@users.noreply.github.com>
2021-05-25 19:17:31 -0400

    Moving html into jquery functions

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-    console.log("here");
+    
+    // main function to use jquery to append questions
+    $(".page").append(
+        `<div class="name">
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name"><br><br>
+        </div>`
+
+    )
 });
